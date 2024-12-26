59f97d0d378b8b020312295605e705c2362c3758
Abijeeth mantha <97285678+abhijeeth-mantha@users.noreply.github.com>
2024-12-26 05:33:47 +0000

    deleted:    members.js

--- members.js
+++
@@ -1,8 +0,0 @@
-function skillsMember() {
-  var members = document.getElementsByClassName("member");
-  for (var i = 0; i < members.length; i++) {
-    var member = members[i];
-    var skills = member.getElementsByClassName("skills")[0];
-    skills.style.display = "none";
-  }
-}
